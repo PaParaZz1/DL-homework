cfa63f6c497579491e5bf3549e3b34a9d7c80a68
niuyazhe <niuyazhe@sensetime.com>
2019-04-20 18:08:18 +0800

    add basic attack

---
+++ 03-adversarial-examples-pytorch/attack.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# vim: ts=4 sw=4 sts=4 expandtab
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class AttackModel(nn.Module):
+
+    def __init__(self, model, mean, std, epsilon=1e-7):
+        super(AttackModel, self).__init__()
+        self.model = model
+        self.mean = mean
+        self.std = std
+        self.epsilon = epsilon
+
+    def forward(self, pre_noise, x):
+        noise = 10 * F.tanh(pre_noise)
+        x_noise = x + noise
+        x_clip = x.clamp(0, 255)
+        round_term = (x_clip//1 - x_clip).requires_grad_(False)
+        x_round = x_clip + round_term
+        x_norm = (x_round - self.mean) / (self.std + self.epsilon)
+        x = self.model(x)
+        return x
+
+    def evaluate(self, x):
+        x = (x - self.mean)/(self.std + self.epsilon)
+        x = self.model(x)
+        return x
